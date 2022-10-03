5b1d282916edb6f2642f18084e5e494b29b9714e
AndrewKamSki <104537278+AndrewKamSki@users.noreply.github.com>
2022-10-03 07:14:01 -0400

    Added code to write to the db.json file, program having trouble displaying notes to the side-bar

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const db = require('./db/db.json');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 
@@ -27,6 +28,40 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// GET Route for reading db.json file
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/db/db.json'))
+});
+
+// POST Route for receiving new note and saving to db.json file
+app.post('/api/notes', (req, res) => {
+  console.log(req.body);
+  const {title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(newNote);
+        fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) =>
+          err ? console.error(err) : console.info(`\nData written to db.json`)
+      );
+      };
+    });
+    res.json('New note added successfully');
+  } else {
+    res.error('Error adding note');
+  }
+});
+
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
